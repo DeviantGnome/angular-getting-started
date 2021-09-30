5f9456be56b426df3e601198093488a22879a0e9
DeviantGnome <patrick@rutledges.net>
2021-09-29 20:31:52 -0500

    1 file modified

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -10,7 +10,7 @@ import { Product, products } from '../products';
 })
 export class ProductDetailsComponent implements OnInit {
 
-  product: Product;
+  product: Product | undefined;
 
   constructor(private route: ActivatedRoute) { }
 
